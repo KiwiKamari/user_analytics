2933a366a3bfdf2b9d9c71aa69349d6f7aa42400
KiwiKamari <gerota53@gmail.com>
2026-04-24 16:56:54 -0600

    Adding files

---
+++ queries/qry_sales_stages.sql
@@ -0,0 +1,15 @@
+-- Define sales funnel and the different stages
+
+WITH funnel_stages AS (
+  SELECT
+    COUNT(DISTINCT CASE WHEN event_type = 'page_view' THEN user_id END) AS stage_1_views,
+    COUNT(DISTINCT CASE WHEN event_type = 'add_to_cart' THEN user_id END) AS stage_2_cart,
+    COUNT(DISTINCT CASE WHEN event_type = 'checkout_start' THEN user_id END) AS stage_3_checkout,
+    COUNT(DISTINCT CASE WHEN event_type = 'payment_info' THEN user_id END) AS stage_4_payment,
+    COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) AS stage_5_purchase
+  FROM `sql_practice.user_events`
+)
+
+SELECT  
+  *
+FROM funnel_stages;
---
+++ queries/qry_traffic_sources_rates.sql
@@ -0,0 +1,23 @@
+-- Funnel by source
+
+WITH source_funnel AS (
+  SELECT
+    traffic_source,
+    COUNT(DISTINCT CASE WHEN event_type = 'page_view' THEN user_id END) AS views,
+    COUNT(DISTINCT CASE WHEN event_type = 'add_to_cart' THEN user_id END) AS carts,
+    COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) AS purchases
+  FROM `sql_practice.user_events`
+  GROUP BY traffic_source
+)
+
+SELECT
+  traffic_source,
+  views,
+  carts,
+  purchases,
+
+  ROUND(carts * 100 / views) AS cart_conversion_rate,
+  ROUND(purchases * 100 / carts) AS cart_to_purchase_conversion_rate,
+  ROUND(purchases * 100 / views) AS purchases_conversion_rate
+FROM source_funnel
+ORDER BY purchases_conversion_rate DESC;
